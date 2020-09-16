c1d476c2df61aa862d57117dad91a7659ea85ac6
Raushan <iamnotbutyou@gmail.com>
2020-09-17 01:52:14 +0530

    card added for chat, test pages

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,31 +1,109 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import {Link,useHistory} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import './CSS/Test.css';
 import {SUBJECTS} from '../shared/subjects';
 
+import Card from './testCard';
 
-export class TestComponent extends Component {
-      render() {
-            const displaysubject=SUBJECTS.map((subject)=>{
-                  return (
-                       
-                        <Link className="link" to={`/test/${subject.name}`}>
-                        <div className="t-subject" style={{backgroundColor:subject.col}}> 
-                              <h3>{subject.name.toUpperCase()}</h3>
-                        </div>
-                        </Link>
-                      
-                  );
-            })
+const useStyles = makeStyles(() => ({
+      "clearfix:after": {
+		content: "",
+		display: "table",
+		clear: "both" 
+	},
+	trow:{
+		width:"100%",
+		height:"100%",
+	},
+	trow1:{
+		width:"100%",
+		height:"100%",
+		marginTop:"3%",
+
+		'&:after':{
+			content: '" "',
+			display: "table",
+			clear: "both" 
+		}
+		
+	},
+	tsub1:{
+		float:"left",
+		marginLeft:"4%",
+		height:"60%",
+		width:"20%",
+		marginTop:"6%"
+	},
+	tsub2:{
+		float:"left",
+		marginLeft:"5%",
+		height:"60%",
+		width:"20%",
+		marginTop:"6%"
+	},
+	tsub3:{
+		float:"left",
+		marginLeft:"5%",
+		height:"60%",
+		width:"20%",
+		marginTop:"6%"
+      },
+      tsub4:{
+		float:"left",
+		marginLeft:"5%",
+		height:"60%",
+		width:"20%",
+		marginTop:"6%"
+	}
+}));
+
+function TestComponent(){
+      
+      const history=useHistory();
+            
+      const displaysubject=SUBJECTS.map((subject)=>{
             return (
-                  <div className="comp">
-                  <div className="t-test">
-                        {displaysubject}
+                  
+                  <Link className="link" to={`/test/${subject.name}`}>
+                  <div className="t-subject" style={{backgroundColor:subject.col}}> 
+                        <h3>{subject.name.toUpperCase()}</h3>
                   </div>
+                  </Link>
+                  
+            );
+      })
+
+      const handlesubmit=(e)=>{
+            const subject=e.target.name;
+            history.push("/test/"+subject);
+
+      };
+      
+      const styles = useStyles();
+      
+      return (
+            <div className={styles.trow}>
+            
+            <div className={styles.trow1}>
+                  <div className={styles.tsub1}>
+                  <Card  handlesubmit={handlesubmit} sub={"Maths"} content={'Dear math, I am sick and tired of finding your "x" just accept the fact that she is gone Move On, DuDe'}/>
                   </div>
-            )
-      }
+                  <div className={styles.tsub2}>
+                  <Card  handlesubmit={handlesubmit} sub={"Chemistry"} content={"not to get technical ... but according to chemistry, alcohol is a solution"}/>
+                  </div>
+                  <div className={styles.tsub3}>
+                  <Card  handlesubmit={handlesubmit} sub={"Biology"} content={'Biology is the study of the complex things in the Universe.'}/>
+                  </div>
+                  <div className={styles.tsub4}>
+                  <Card  handlesubmit={handlesubmit} sub={"Physics"} content={"i am not lazy i'm overflowing with potential energy"}/>
+                  </div>
+            </div>
+            
+            </div>
+      )
+      
 }
 
 export default TestComponent
--- src/components/queryComponent.js
+++ src/components/queryComponent.js
@@ -1,8 +1,71 @@
 import React , { useState, useEffect } from 'react';
 import { Switch, Route, Redirect, NavLink, Link, useHistory} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import axios from "axios";
 import Client from "./client/client"
+
+import Card from './Card';
+
+const useStyles = makeStyles(() => ({
+      "clearfix:after": {
+		content: "",
+		display: "table",
+		clear: "both" 
+	},
+	row:{
+		width:"100%",
+		height:"100%",
+	},
+	row1:{
+		width:"100%",
+		height:"100%",
+		marginTop:"3%",
+
+		'&:after':{
+			content: '" "',
+			display: "table",
+			clear: "both" 
+		}
+		
+	},
+	row2:{
+		width:"25%",
+		height:"40%",
+		marginTop:"3%",
+		marginLeft:"37%",
+		marginBottom:"14%"
+	},
+	csub1:{
+		float:"left",
+		marginLeft:"4%",
+		height:"50%",
+		width:"25%",
+		marginTop:"8%"
+	},
+	csub2:{
+		float:"left",
+		marginLeft:"9%",
+		height:"100%",
+		width:"25%"
+	},
+	csub21:{
+		height:"37%",
+		width:"100%"
+	},
+	csub22:{
+		marginTop:"22%",
+		height:"37%",
+		width:"100%"
+	},
+	csub3:{
+		float:"left",
+		marginLeft:"11%",
+		height:"50%",
+		width:"25%",
+		marginTop:"8%"
+	}
+}));
 
 
 export default function Query(props){
@@ -14,9 +77,9 @@ export default function Query(props){
 		const subject=e.target.name;
 		
 		var number={ 
-			"physics": "+919644012345",
-			"maths": "+917994012369",
-			"chemistry": "+918344012152" 
+			"Physics": "+919644012345",
+			"Maths": "+917994012369",
+			"Chemistry": "+918344012152" 
 		}
 	
 		var tNumber=number[subject]
@@ -34,12 +97,27 @@ export default function Query(props){
 
 	};
 
+	const styles = useStyles();
+
 	return (
-		<div>
-			<button onClick={handlesubmit} name="physics">Physics</button>
-			<button onClick={handlesubmit} name="chemistry">Chemistry</button>
-			<button onClick={handlesubmit} name="maths">Maths</button>
+	<div className={styles.row}>
+		<div className={styles.row1}>
+			<div className={styles.csub1}>
+			<Card  handlesubmit={handlesubmit} sub={"Maths"} content={'Dear math, I am sick and tired of finding your "x" just accept the fact that she is gone Move On, DuDe'}/>
+			</div>
+			<div className={styles.csub2}>
+				<div className={styles.csub21}>
+				<Card  handlesubmit={handlesubmit} sub={"Chemistry"} content={"not to get technical ... but according to chemistry, alcohol is a solution"}/>
+				</div>
+				<div className={styles.csub22}>
+				<Card  handlesubmit={handlesubmit} sub={"Biology"} content={'Biology is the study of the complex things in the Universe.'}/>
+				</div>
+			</div>
+			<div className={styles.csub3}>
+			<Card  handlesubmit={handlesubmit} sub={"Physics"} content={"i am not lazy i'm overflowing with potential energy"}/>
+			</div>
 		</div>
+	</div>
 	);
 }
 
